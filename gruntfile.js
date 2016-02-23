b7f8198c027ea640008ecc43e2a534a957abe094
Ivan Loyola <iLoyola@Ivans-Mac-mini.local>
2016-02-22 20:18:09 -0500

    Grunt, Jekyll and LibSaSass

---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Show elapsed time after tasks run to visualize performance
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
+
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_sass',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_sass/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
+    });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'shell:jekyllServe'
+    ]);
+
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild',
+        'sass'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
+
+};
